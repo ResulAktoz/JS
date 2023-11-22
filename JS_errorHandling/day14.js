c4d1fadc1d23d68a8160f207b009b66ff3ab44ed
Resul Aktoz <aktozresul@gmail.com>
2023-11-22 14:21:08 +0300

    ErrorHandling

---
+++ JS_errorHandling/day14.js
@@ -0,0 +1,24 @@
+/*try{
+    let firstName = 'Resul'
+    let lastName = 'Aktoz'
+    fullName = firstName + ' ' + lastName
+
+    throw new Error('Bir hata var!')
+}catch(e){
+    console.log(e.name)
+    console.table(e)
+}
+
+console.log(fullName)*/
+
+/*let age = prompt('Kac yasindasin?')
+
+try{
+    if(age>24) throw new Error('Dogru degil')
+}catch(e){
+    //alert('yasiniz ile ilgili bilgi dogru degil.')
+    alert(e.message)
+}*/
+
+// console.log(5x5) synt error
+
