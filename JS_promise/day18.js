5d7eebb5eca3b9ca8cfdee29af82ae621153a1b1
Resul Aktoz <aktozresul@gmail.com>
2023-12-03 14:34:09 +0300

    JSasync_promise

--- JS_promise/day18.js
+++ JS_promise/day18.js
@@ -16,9 +16,10 @@ const callback= (err,data) => {
 }
 test(callback)*/
 
-const promise = new Promise ((resolve, reject) => {
+/*const promise = new Promise ((resolve, reject) => {
     setTimeout(() => {
-        resolve('islem basarili')
+        //resolve('islem basarili')
+        reject('islem basarisiz')
     }, 2500)
 
     //resolve('islem basarili')
@@ -28,4 +29,33 @@ promise.then((data) => {
     console.log(data)
 }).catch (err => {
     console.warn(err)
+})*/
+
+const API_URL ='https://jsonplaceholder.typicode.com/posts'
+
+/*(() => {
+    const res = await fetch(API_URL)
+    console.log(res)
+})*/
+
+/*fetch(API_URL)
+    //, {
+    //method :'POST',
+    //body : FormData         //sunucuya değer göndermek istersek body ile gönderilecek.
 })
+
+    .then(res => res.json()
+    .then(data => console.log(data))) // istek atılan yerdeki bütün datalar gelir.
+    .catch (err => console.log(err))*/
+
+async function getPosts() {
+    try{
+        const response =  await fetch(API_URL)
+        const data = await response.json()
+        console.log(data)
+    }catch(err){
+            console.log(err)
+    }
+}
+getPosts()
+
