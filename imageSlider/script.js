2e1d7161fda12b58cda4878cd7efba8323a7cb62
Resul Aktoz <aktozresul@gmail.com>
2024-03-24 14:56:12 +0300

    imageSlider
    setInterval and resetInt. added

---
+++ imageSlider/script.js
@@ -0,0 +1,42 @@
+const images = document.getElementById('imgs')
+const prevBtn = document.getElementById('left')
+const nextBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run () {
+    idx++
+
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    }else if (idx < 0) {
+        idx = img.length - 1
+    }
+    images.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+
+    interval = setInterval(run, 2000)
+}
+
+prevBtn.addEventListener('click', () => {
+    idx--
+
+   changeImage()
+})
+nextBtn.addEventListener('click', () => {
+   idx++
+
+   changeImage()
+
+   resetInterval()
+})
