399d3f48d657247de29c0cb9484ea2536f8b4b40
Resul Aktoz <aktozresul@gmail.com>
2024-03-27 21:59:39 +0300

    passwordStrength
    blur effect will be fixed

---
+++ passwordStrength/script.js
@@ -0,0 +1,20 @@
+const password = document.getElementById("password");
+const background = document.getAnimations("background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  //   console.log(input);
+  const length = input.length;
+  //   console.log(20 - length*2);
+
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
+
+password.addEventListener("mouseover", () => {
+  password.setAttribute("placeholder", "Your password should be 10 letters");
+});
+
+password.addEventListener("mouseleave", () => {
+  password.setAttribute("placeholder", "Enter your password");
+});
